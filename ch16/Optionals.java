567e19a86b4ac2284601f80ecb125fe66983c03c
Oscar Huerta <ohuerta@indeed.com>
2024-06-10 19:21:07 -0500

    Improved many errors and linting issues

--- ch16/Optionals.java
+++ ch16/Optionals.java
@@ -1,5 +1,6 @@
 package ch16;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.OptionalInt;
@@ -16,16 +17,16 @@ public class Optionals {
 
     public static void createOptionals() {
         Optional<Object> opt1 = Optional.empty();
-        opt1.ifPresent(o -> System.out.println("opt1: " + o));
+        opt1.ifPresent(System.out::println);
 
         Optional<Integer> opt2 = Optional.of(23);
-        opt2.ifPresent(o -> System.out.println("opt2: " + o));
+        opt2.ifPresent(System.out::println);
 
-        Optional<Integer> opt3 = Optional.ofNullable(23);
-        opt3.ifPresent(o -> System.out.println("opt3: " + o));
+        Optional<Integer> opt3 = Optional.of(23);
+        opt3.ifPresent(System.out::println);
 
-        Optional<Object> opt4 = Optional.ofNullable(null);
-        opt4.ifPresent(o -> System.out.println("opt4: " + o));
+        Optional<Object> opt4 = Optional.empty();
+        opt4.ifPresent(System.out::println);
         if (opt4.isEmpty()) {
             System.out.println("opt4 is empty!"); // opt4 is empty!
         }
@@ -33,13 +34,15 @@ public class Optionals {
 
     public static void doOptionalPrimitiveAverage() {
         OptionalDouble optAvg = IntStream.rangeClosed(1, 10).average();
-        optAvg.ifPresent(d -> System.out.println(d)); // 5.5
-        System.out.println(optAvg.getAsDouble()); // 5.5
-        double dblAvg = optAvg.orElseGet(() -> Double.NaN);
+        optAvg.ifPresent(System.out::println); // 5.5
+        if (optAvg.isPresent()) {
+            System.out.println(optAvg.getAsDouble()); // 5.5
+        }
+        double dblAvg = optAvg.orElse(Double.NaN); // Directly use orElse
         System.out.println(dblAvg); // 5.5
 
         OptionalInt optInt = OptionalInt.of(35);
-        int age = optInt.orElseGet(() -> 0);
+        int age = optInt.orElse(0);
         System.out.println(age); // 35
         System.out.println(optInt.getAsInt()); // 35
     }
@@ -49,7 +52,7 @@ public class Optionals {
         optSK.ifPresent(System.out::println); // SK
 
         Optional<String> emptyOpt = howToDealWithNull(null);
-        System.out.println(emptyOpt.orElseGet(() -> "Empty optional")); // Empty optional
+        System.out.println(emptyOpt.orElseGet(Optionals::getEmptyOptionalMessage)); // Empty optional
     }
 
     public static Optional<String> howToDealWithNull(String param) {
@@ -57,58 +60,21 @@ public class Optionals {
     }
 
     public static void doOptionalAPI() {
-        Optional<Double> valueInOptional = Optional.ofNullable(60.0);
-        if (valueInOptional.isPresent()) {
-            System.out.println(valueInOptional.get()); // 60.0
-        }
+        Optional<Double> valueInOptional = Optional.of(60.0);
         valueInOptional.ifPresent(System.out::println); // 60.0
         System.out.println(valueInOptional.orElse(Double.NaN)); // 60.0
 
-        Optional<Double> emptyOptional = Optional.ofNullable(null);
+        Optional<Double> emptyOptional = Optional.empty();
         System.out.println(emptyOptional.orElse(Double.NaN)); // NaN
         System.out.println(emptyOptional.orElseGet(Math::random)); // Random number
         try {
-            System.out.println(emptyOptional.orElseThrow()); // NoSuchElementException
-        } catch (Exception e) {
+            System.out.println(emptyOptional.orElseThrow(NoSuchElementException::new)); // NoSuchElementException
+        } catch (NoSuchElementException e) {
             System.out.println("Exception caught: " + e);
         }
     }
+
+    private static String getEmptyOptionalMessage() {
+        return "Empty optional";
+    }
 }
-
-/*
-save
-    public static void doOptionalPrimitiveAverage(){
-        OptionalDouble optAvg = IntStream.rangeClosed(1, 10)
-                                    .average();
-        // DoubleConsumer - functional interface; functional method is:
-        //    void accept(double value)
-        optAvg.ifPresent((d) -> System.out.println(d));// 5.5
-        System.out.println(optAvg.getAsDouble());// 5.5
-        // DoubleSupplier - functional interface; functional method is:
-        //    double getAsDouble()
-        System.out.println(optAvg.orElseGet(() -> Double.NaN));// 5.5
-    }
-
-    // a long way to calculate average (just for showing Optional)
-    public static Optional<Double> calcAverage(int... grades){
-        if(grades.length == 0) return Optional.empty();
-        int total=0;
-        for(int grade:grades) total += grade;
-        return Optional.of((double)total / grades.length);
-    }
-    public static void doOptionalAverage(){
-       Optional<Double> valueInOptional = calcAverage(50, 60, 70);
-        if(valueInOptional.isPresent()){
-            System.out.println(valueInOptional.get()); // 60.0
-        }
-        valueInOptional.ifPresent(System.out::println);// 60.0
-        System.out.println(valueInOptional.orElse(Double.NaN)); // 60.0
-
-        Optional<Double> emptyOptional = calcAverage();// will return an empty Optional
-        System.out.println(emptyOptional.orElse(Double.NaN)); // NaN
-        System.out.println(emptyOptional.orElseGet(() -> Math.random())); // 0.8524556508038182
-//        System.out.println(emptyOptional.orElseThrow()); // NoSuchElementException
-        System.out.println(emptyOptional.orElseThrow(() -> new RuntimeException())); // RuntimeException
-    }
-
- */
